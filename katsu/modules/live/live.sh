606723290332da2fb69f3fbc079097b26634ef47
owen@fyralabs.com <owen@fyralabs.com>
2025-07-23 20:04:16 -0500

    Initial shades configs

--- katsu/modules/live/live.sh
+++ katsu/modules/live/live.sh
@@ -49,7 +49,7 @@ systemctl disable systemd-networkd-wait-online
 systemctl disable openvpn-client@\*.service
 systemctl disable openvpn-server@\*.service
 
-# END options from upstream https://pagure.io/fedora-kickstarts/blob/f41/f/fedora-live-base.ks
+# END options from upstream https://pagure.io/fedora-kickstarts/blob/f42/f/fedora-live-base.ks
 
 # Remove machine-id on pre generated images
 rm -f /etc/machine-id
