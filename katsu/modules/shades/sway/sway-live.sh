62367fe79a395f78bdbb5c30b41c30ec7b75131e
owen@fyralabs.com <owen@fyralabs.com>
2025-07-23 20:04:16 -0500

    Initial shades configs

---
+++ katsu/modules/shades/sway/sway-live.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+# set default GTK+ theme for root (see #683855, #689070, #808062)
+# cat >/root/.gtkrc-2.0 <<EOF
+# include "/usr/share/themes/Adwaita/gtk-2.0/gtkrc"
+# include "/etc/gtk-2.0/gtkrc"
+# gtk-theme-name="Adwaita"
+# EOF
+# mkdir -p /root/.config/gtk-3.0
+# cat >/root/.config/gtk-3.0/settings.ini <<EOF
+# [Settings]
+# gtk-theme-name = Adwaita
+# EOF
+
+# set livesys session type
+# sed -i 's/^livesys_session=.*/livesys_session="kde"/' /etc/sysconfig/livesys
