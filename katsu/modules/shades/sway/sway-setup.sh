fb7c0c8cd30dcf18f903a5bb433e9652d5c4c522
Owen <owen@fyralabs.com>
2025-09-08 18:19:55 -0500

    Script no work

--- katsu/modules/shades/sway/sway-setup.sh
+++ katsu/modules/shades/sway/sway-setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 
 dnf remove gnome-* cinnamon-* -y
-ls -la
-# Sway
-cp katsu/modules/shades/sway/default-configs/sway-config /etc/sway/config
+# ls -la
+# # Sway
+# cp katsu/modules/shades/sway/default-configs/sway-config /etc/sway/config
 
-# Waybar
-cp katsu/modules/shades/sway/default-configs/waybar-config.jsonc /etc/xdg/waybar/config.jsonc
-cp katsu/modules/shades/sway/default-configs/waybar-style.css /etc/xdg/waybar/style.css
+# # Waybar
+# cp katsu/modules/shades/sway/default-configs/waybar-config.jsonc /etc/xdg/waybar/config.jsonc
+# cp katsu/modules/shades/sway/default-configs/waybar-style.css /etc/xdg/waybar/style.css
 
-# Rofi
-cp katsu/modules/shades/sway/default-configs/rofi-config.rasi ~/.config/rofi/config.rasi
+# # Rofi
+# cp katsu/modules/shades/sway/default-configs/rofi-config.rasi ~/.config/rofi/config.rasi
+
+
+# I will figure out how to do this later
