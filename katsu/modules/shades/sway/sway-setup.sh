16c39104bd5f1db2dfeef87e6032d8f6e14c077c
j <105894089+Its-J@users.noreply.github.com>
2025-09-08 12:44:06 -0500

    added default files and a bit more

--- katsu/modules/shades/sway/sway-setup.sh
+++ katsu/modules/shades/sway/sway-setup.sh
@@ -5,4 +5,12 @@ dnf remove gnome-* cinnamon-* -y
 # make directory for screenshots to end up in
 mkdir ~/Media
 
+# Sway
 cp katsu/modules/shades/sway/config /etc/sway/config
+
+# Waybar
+cp katsu/modules/shades/sway/config.jsonc /etc/xdg/waybar/
+cp katsu/modules/shades/sway/style.css /etc/xdg/waybar/
+
+# Rofi
+cp katsu/modules/shades/sway/config.rasi ~/.config/rofi/
