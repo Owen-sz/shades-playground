b352b3f49381f4432934f5ac89a16ef6924de7f3
Owen Zimmerman <123591347+Owen-sz@users.noreply.github.com>
2025-09-08 11:12:44 -0500

    Clean up sway-setup.sh

--- katsu/modules/shades/sway/sway-setup.sh
+++ katsu/modules/shades/sway/sway-setup.sh
@@ -2,11 +2,5 @@
 
 dnf remove gnome-* cinnamon-* -y
 
-CONFIG_FILE="katsu/modules/shades/sway/config"
-DEST_DIR="/etc/sway/config"
-cp "$CONFIG_FILE" "$DEST_DIR"
-# cp config /etc/sway/config
+cp katsu/modules/shades/sway/config /etc/sway/config
 
-#
-# /etc/xdg/waybar/config.jsonc
-# /etc/xdg/waybar/style.css
