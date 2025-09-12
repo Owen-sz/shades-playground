b89346b580c959e5ed574457c4ab00d86ff53b87
j <105894089+Its-J@users.noreply.github.com>
2025-09-11 21:04:59 -0500

    shebang and fix shutdown commands

--- katsu/modules/shades/sway/default-configs/rofi-shutdown-script.sh
+++ katsu/modules/shades/sway/default-configs/rofi-shutdown-script.sh
@@ -7,7 +7,6 @@ action=$(printf "Log out\nShut Down\nReboot\nCancel" | rofi -dmenu -l 4)
 if [[ $action == "Log out" ]]; then
     confirm=$(printf "Yes\nNo" | rofi -dmenu -l 2 -mesg "Are you sure you want to log out?")
     if [[ $confirm == "Yes" ]]; then
-        ## might be better to have the intended log out commands for respective wm
         if [[ $DESKTOP_SESSION == "sway" ]]; then
             swaymsg exit
         fi
@@ -18,7 +17,7 @@ if [[ $action == "Log out" ]]; then
 elif [[ $action == "Shut Down" ]]; then
     confirm=$(printf "Yes\nNo" | rofi -dmenu -l 2 -mesg "Are you sure you want to shut down?")
     if [[ $confirm == "Yes" ]]; then
-        systemctl poweroff
+        systemctl shutdown -r now 'User Shutdown Action'
     else
         exit
     fi
@@ -26,7 +25,7 @@ elif [[ $action == "Shut Down" ]]; then
 elif [[ $action == "Reboot" ]]; then
     confirm=$(printf "Yes\nNo" | rofi -dmenu -l 2 -mesg "Are you sure you want to reboot?")
     if [[ $confirm == "Yes" ]]; then
-        reboot
+        systemctl reboot --now
     else
         exit
     fi
